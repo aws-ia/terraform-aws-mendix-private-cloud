00dc97dd206d009467745b194dc4aef3a20a2775
Anton Popovichenko <anton.popovichenko@mendix.com>
2023-08-04 16:41:09 +0200

    Fix formatting

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,11 +19,11 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
+    "kubernetes.io/role/elb"                    = "1"
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
 }
